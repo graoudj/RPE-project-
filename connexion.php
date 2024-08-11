0c35650ce3c37f727bedf714c5bd5d9671ff9a8a
Nazim (ACDC) <41114547+graoudj@users.noreply.github.com>
2024-08-11 19:29:18 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,10 +1,7 @@
 <!-- Projet RPE_Bilboq -->
 <?php
 class connect extends PDO {
-const HOST="db5016158432.hosting-data.io:3306";
-const DB="dbs13153657";
-const USER="dbu1453259";
-const PSW="RAM@46140_Bilboquet";
+
       public function __construct(){
    //  try{
      //     parent::__construct("mysql:dbname=".self::DB.";host=".self::HOST,self::USER,self::PSW);
